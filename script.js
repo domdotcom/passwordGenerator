f5f69c03f5fff5f72f1660e3a9f38529d64873d5
Dominika <dominikaperkowska@gmail.com>
2020-05-20 22:12:13 -0400

    updated js file

--- script.js
+++ script.js
@@ -1,55 +1,52 @@
-const form = document.getElementById('form')
-const lengthEl = document.getElementById('characters')
-const lowercaseEl = document.getElementById('includeLowerCase')
-const uppercaseEl = document.getElementById('includeUpperCase')
-const numbersEl = document.getElementById('includeNumbers')
-const symbolsEl = document.getElementById('includeSymbols')
-const displayEl = document.getElementById('display')
-
-const uppercaseChar = arrayCharCodes(65,90)
-const lowercaseChar = arrayCharCodes(97,122)
-const numChar = arrayCharCodes(48,57)
-const symChar = arrayCharCodes(33,47)
-.concat(
-    arrayCharCodes(58,64)
-).concat(
-    arrayCharCodes(91,96)
-).concat(
-    arrayCharCodes(123,126)
-)
+// linking elements in html
+const formElement = document.getElementById('inputForm')
+const lengthElement = document.getElementById('characters')
+const uppercaseElement = document.getElementById('includeUppercase')
+const lowercaseElement = document.getElementById('includeLowercase')
+const numberElement = document.getElementById('includeNumbers')
+const symbolElement = document.getElementById('includeSymbols')
+const display = document.getElementById('displayBox')
 
 // form listening event
-form.addEventListener('submit', e => {
-    const length = lengthEl.value
-    const symbols = symbolsEl.checked
-    const numbers = numbersEl.checked
-    const uppercase = uppercaseEl.checked
-    const lowercase = lowercaseEl.checked
+formElement.addEventListener('submit', e => {
+    const length = lengthElement.value
+    const uppercase = uppercaseElement.checked
+    const lowercase = lowercaseElement.checked
+    const numbers = numberElement.checked
+    const symbols = symbolsElement.checked
     const password = generatePassword(length, uppercase, lowercase, symbols, numbers)
-    displayEl.innerHTML = password
 })
+// char code variables
+const lowerCharCodes = arrayCharCodes(65,90)
+const upperCharCodes = arrayCharCodes(97,122)
+const numberCharCodes = arrayCharCodes(48,57)
+const symbolCharCodes = arrayCharCodes(33,47)
+    .concat(arrayCharCodes(58,64))
+        .concat(arrayCharCodes(91,96))
+            .concat(arrayCharCodes(123,126))
 
+// char codes loop function
 
-function generatePassword(length,uppercase, lowercase, symbols, numbers){
-  let charCodes = lowerCaseChar
-  if (uppercase) charCodes = charCodes.concat(uppercaseChar)
-  if (symbols) charCodes = charCodes.concat(symchar)
-  if (numbers) charCodes = numChar.concat(numChar)
-
-  const passwordCharacters = []
-  for (let i = 0; i < characterAmount; i++) {
-      const characterCode = charCode[Math.floor(Math.random()* charCodes.length)]
-      passwordCharacters.push(String.fromChardCode(characterCode))
-  }
-  return passwordCharacters.join('')
-}
-
-function arrayCharCodes(low,high) {
+function arrayCharCodes(min,max){
     const array = []
-    for (let i = low; i <= high; i++) {
+    for (let i = min; i<= max; i++){
         array.push(i)
     }
     return array
 }
+// password genertor function
 
+function generatePassword(length, uppercase, lowercase, symbols, numbers){
+    let charCodes = lowerCharCodes
+    if (uppercase) charCodes = charCodes.concat(upperCharCodes)
+    if (numbers) charCodes = charCodes.concat(numberCharCodes)
+    if (symbols) charCodes = charCodes.concat(symbolCharCodes)
+
+    const characters = []
+    for (let i =0; i <length; i++){
+        const char = charCodes[Math.floor(Math.random() * charCodes.length)]
+        characters.push(String.fromCharCode(char))
+    }
+    return characters.join('')
+}
 
